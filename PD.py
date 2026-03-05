d4b903bf0ee736da9639ac7b3319731b747ff9b3
Dan1la2 <z1herhht@gmail.com>
2026-03-05 08:33:05 +0200

    05.03.2026 Pd

---
+++ PD.py
@@ -0,0 +1,17 @@
+#vards = input("Ievadi savu vardu:")
+#print("Sveiki!", vards )
+
+#age = int(input("Ievadi vecumu: "))
+
+#if age  <= 18:
+
+#  print("Nepilngadīgs")
+
+a = 3
+b = 5
+
+
+def summa (a, b):
+    return a * b
+print(summa(1,1))
+
